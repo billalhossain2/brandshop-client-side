6dd336516331ab8cffabcdd61b248f55650622c2
billal-webdev <billal-webdev@gmail.com>
2023-10-19 16:53:15 +0600

    make cart button functional to add product to cart

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import useTitle from "../../hooks/useTitle";
 import { useParams } from "react-router-dom";
 import Spinner from "../../components/Spinner";
+import { toast } from "react-toastify";
 
 const ProductDetails = () => {
   useTitle("Product Details - Tech Store")
@@ -19,6 +20,24 @@ const ProductDetails = () => {
     })
     .catch(error => console.log(error))
   }, [])
+
+  const handleAddToCart = ()=>{
+    const {image, name, brand_name, type, price, rating, description} = product;
+    fetch('http://localhost:9000/cart', {
+      method:"POST",
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify({image, name, brand_name, type, price, rating, description})
+    })
+    .then(res => res.json())
+    .then(result => {
+      if(result.acknowledged){
+        toast("Successfully added to cart")
+      }
+    })
+    .catch(error => console.log(error))
+  }
 
   return (
     <>
@@ -37,15 +56,15 @@ const ProductDetails = () => {
         <div className="w-full md:w-1/2 lg:w-2/3 px-4 mb-8">
           <h2 className="text-2xl font-semibold mb-2">{product.name}</h2>
           <p className=" text-[#F5921D] text-2xl"><span className="font-bold">Price:</span>{product.price}</p>
-          <p><span className="font-bold">Brand:</span> {product.brand}</p>
+          <p><span className="font-bold">Brand:</span> {product.brand_name}</p>
           <p><span className="font-bold">Product Type:</span> {product.type}</p>
           <p><span className="font-bold">Rating:</span> {product.rating}</p>
-          <p><span className="font-bold">Availability:</span> {product?.availability}</p>
-          <p><span className="font-bold">Color:</span> {product?.color}</p>
-          <p><span className="font-bold">Warranty:</span> {product?.warranty}</p>
-          <p><span className="font-bold">Shipping:</span> {product?.shipping}</p>
+          <p><span className="font-bold">Availability:</span> {product.availability || "Not Available"}</p>
+          <p><span className="font-bold">Color:</span> {product.color || "Not Available"}</p>
+          <p><span className="font-bold">Warranty:</span> {product.warranty || "Not Available"}</p>
+          <p><span className="font-bold">Shipping:</span> {product.shipping || "Not Available"}</p>
           <p className="text-gray-600 mb-4">{product.description}</p>
-          <button className="bg-[#F5921D] text-white px-4 py-2 rounded-md mt-4">
+          <button onClick={handleAddToCart} className="bg-[#F5921D] text-white px-4 py-2 rounded-md mt-4">
             Add to Cart
           </button>
         </div>
